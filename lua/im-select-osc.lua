b08080dc3eb4c6ba4ea047aca04543f9e309c755
mkdir700 <mkdir700@gmail.com>
2023-06-03 17:24:23 +0800

    u

--- lua/im-select-osc.lua
+++ lua/im-select-osc.lua
@@ -16,12 +16,13 @@ local function write(osc1337)
   local success = false
   -- if vim.fn.filwritable("/dev/fd/2") == 1 then
   --   success = vim.fn.writefile({ osc1337 }, "/dev/fd/2", "b") == 0
-  -- elseif vim.fn.has("nvim") then
-  --   success = vim.fn.chansend(vim.api.nvim_get_var("stderr"), osc1337) > 0
+  -- if vim.fn.has("nvim") then
+  io.stderr:write(osc1337)
+
   -- else
-  vim.cmd("silent! !echo " .. vim.fn.shellescape(osc1337))
-  vim.cmd("redraw!")
-  success = true
+  --   vim.cmd("silent! !echo " .. vim.fn.shellescape(osc1337))
+  --   vim.cmd("redraw!")
+  --   success = true
   -- end
   return success
 end
@@ -31,7 +32,6 @@ end
 local function echo(msg)
   vim.api.nvim_echo({ { msg, "Normal" } }, false, {})
 end
-
 
 M.IMSelect = function()
   -- echo("\033]1337;Custom=id=" .. M.config.secret .. ":im-select\a")
