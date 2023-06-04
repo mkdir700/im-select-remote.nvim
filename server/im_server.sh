70c90d7ed633a7f8716291c77cadb387f143dd4d
mkdir700 <mkdir700@gmail.com>
2023-06-04 21:32:03 +0800

    update echo info

--- server/im_server.sh
+++ server/im_server.sh
@@ -4,11 +4,11 @@ port=${1:-23333}
 
 while true; do
   nc -l "0.0.0.0" $port | while read line ; do
-      echo "Received: $line"
+      echo "$(date) | Received: $line"
       if [ "$line" = "exit" ]; then
           break
       elif ! eval "$line"; then
-          echo "Failed to execute command: $line" >&2
+        echo "$(date) | Failed to execute command: $line" >&2
       fi
       exec <&3
   done 3<&0
